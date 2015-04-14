1435916c59e6346b678450c3369daa4132581da4
metoray <metoray@gmail.com>
2015-04-14 13:18:35 +0200

    Stored time in a new format

--- AssemblyClock.asm
+++ AssemblyClock.asm
@@ -60,6 +60,8 @@
  .org OC1Aaddr
  rjmp timer1
 
+time_const: .db high(time), low(time), 3, 60, 60, 24
+alarm_const: .db high(alarm_time), low(alarm_time), 2, 60, 24
 
  main:
 	ldi tmp, low(RAMEND)			; reset stack pointer
@@ -93,19 +95,15 @@
 	
 	ldi ZH, high(time)		;point Z reg to time in RAM
 	ldi ZL, low(time)
-	ldi tmp, 3  
-	st Z+, tmp  			;amount of time segments
-	ldi tmp, 4
-	st Z+, tmp				;hours
-	ldi tmp, 59
-	st Z+, tmp				;minutes
 	ldi tmp, 55
 	st Z+, tmp				;seconds
+	ldi tmp, 59
+	st Z+, tmp				;minutes
+	ldi tmp, 4
+	st Z+, tmp				;hours
 	
 	ldi ZH, high(alarm_time)
 	ldi ZL, low(alarm_time)
-	ldi tmp, 2
-	st Z+, tmp
 	clr tmp
 	st Z+, tmp ;hours
 	st Z+, tmp ;minutes
@@ -138,6 +136,8 @@ loop:
 	sbrs int_flags, counter_flag	
 	rjmp loop_blink						; skipped if counter flag is set
 	
+	ldi ZH, high(time_const<<1)
+	ldi ZL, low(time_const<<1)
 	rcall update_time					; update time
 	
 	cbr int_flags, 1<<counter_flag		; clear counter_flag
@@ -222,8 +222,8 @@ loop_update_display:
 	sbrs int_flags, update_display_flag	  
 	rjmp loop							; jump back to loop if display update is turned off
 	
-	ldi ZH, high(time)
-	ldi ZL, low(time)
+	ldi ZH, high(time_const<<1)
+	ldi ZL, low(time_const<<1)
 	rcall display_time
 	
 	cbr int_flags, 1<<update_display_flag
@@ -258,15 +258,15 @@ timer1:
 update_time: ; update current time with one second increase
 	push ZH								; store Z registers 
 	push ZL
-	ldi ZH, high(time+4)				; load Z with adress of time+ 4 bytes
-	ldi ZL, low(time+4)
-	ldi arg, 60							; load 60 for minutes/seconds comparison
-	rcall update_number					; update seconds
-	brcc update_time_end				; if carry cleared no minute update needed
-	rcall update_number					; update minutes
-	brcc update_time_end				; if carry cleared no hour update needed
-	ldi arg, 24							; load 24 for hour comparison
-	rcall update_number
+	lpm YH, Z+
+	lpm YL, Z+
+	lpm tmp, Z+
+update_time_loop:
+	rcall update_number					; update time unit
+	brcc update_time_end
+	dec tmp
+	tst tmp
+	brne update_time_loop
 update_time_end:
 	sbr int_flags, 1<<update_display_flag ;set update display flag since time is updated(inceased one second)
 	pop ZL								; return Z registers
@@ -275,27 +275,38 @@ update_time_end:
 
 
 update_number: 							; time update helper function
-	ld tmp, -Z 							; load second/minute/hour
-	inc tmp	   							; increase time
-	cp tmp, arg							; compare time with 60 or 24
-	clc		   							; clear the carry
-	brne update_number_no_carry 		; if time not equal with 60/24 no update needed
-	clr tmp 							; if time is equal the next time step should be increased and this one cleared
-	sec									; set the carry to indicate that next step should be increased
+	push tmp
+	push arg
+	ld tmp, Y							; load current value
+	lpm arg, Z+							; load max value
+	inc tmp								; increment current value
+	cp tmp, arg							; compare new value to max value
+	brne update_number_no_carry			; if not equal, return without carry
+	clr tmp								; else: reset value
+	sec									; set carry
+	st Y+, tmp							; store new value
+	pop arg
+	pop tmp
+	ret
 update_number_no_carry:
-	st Z, tmp 							; store the new time
+	clc									; clear carry
+	st Y+, tmp							; store new value
+	pop arg
+	pop tmp
 	ret 
-
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;    Display Time    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-display_time:					
-	ld tmp, Z+							; load amount of time segments
+display_time:	
+	lpm YH, Z+
+	lpm YL, Z+
+	lpm tmp, Z+							; load amount of time segments
+	clr arg
+	add YL, tmp
+	adc YH, arg
 	rcall delay_some_ms					; delay a bit
 	push blink							; push blink status register
 	push tmp							; push amount of time segments
@@ -304,7 +315,7 @@ display_time:
 	rcall usart_send					; MULTI: clear sent bytes
 display_time_loop:
 	dec tmp								; lower amount of segments
-	ld arg, Z+							; load time segments
+	ld arg, -Y							; load time segments
 	lsl blink							; shift blink register, will overflow to carry if needed
 	brcs display_time_loop_show         ; if carry set branch to display_time_loop_show 
 	;show blank segment
